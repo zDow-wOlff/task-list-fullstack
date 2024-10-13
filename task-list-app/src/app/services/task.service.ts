f0b6c2ca4ffc965247cc2f9da5324666b4666723
Mohit Raj <89876970+zDow-wOlff@users.noreply.github.com>
2024-10-13 21:35:14 +0530

    Update task.service.ts

--- task-list-app/src/app/services/task.service.ts
+++ task-list-app/src/app/services/task.service.ts
@@ -7,7 +7,7 @@ import { Task } from '../models/task.model';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks'; // Update this with your Flask backend URL
+  private apiUrl = '/.netlify/functions/api/tasks'; // Update this with your Flask backend URL
 
   constructor(private http: HttpClient) { }
 
