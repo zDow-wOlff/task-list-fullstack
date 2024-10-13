84271d80b85f254a647c50f9c90464491057f7ea
Mohit Raj <89876970+zDow-wOlff@users.noreply.github.com>
2024-10-13 23:27:42 +0530

    Update api.py

--- functions/api.py
+++ functions/api.py
@@ -9,7 +9,7 @@ CORS(app, resources={r"/*": {"origins": "https://task-list-fs.netlify.app"}})
 
 # Use SQLite for Netlify deployment
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'tasks.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/tasks.db' + os.path.join(basedir, 'tasks.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -18,7 +18,12 @@ db = SQLAlchemy(app)
 from models import Task
 from routes import *
 
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
 def handler(event, context):
+    create_tables()
     return handle_request(app, event, context)
 
 if __name__ == '__main__':
